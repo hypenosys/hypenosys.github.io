c5ab39e1291a3d5b040980759ceefd2f997ce2df
Axel <axelfernandezcurros@gmail.com>
2026-06-24 13:28:06 +0200

    Change API method from listSessions to getSessions

--- assets/javascript/jules-panel-sessions.js
+++ assets/javascript/jules-panel-sessions.js
@@ -4,7 +4,7 @@
 
 async function refreshDashboard() {
     try {
-        window.julesSessionsCache = await window.julesApi.listSessions();
+        window.julesSessionsCache = await window.julesApi.getSessions();
         renderMetrics();
         renderHistoryTable(window.julesSessionsCache);
         updateKanbanCounts(window.julesSessionsCache);
